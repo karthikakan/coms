0c162c7300cfd3a12cfa9d3de4f0939299531f87
Karthika A Nair <karthika.estrella@gmail.com>
2023-03-07 08:40:35 +0530

    Graph add Button

--- src/pages/GraphPage.js
+++ src/pages/GraphPage.js
@@ -1,10 +1,27 @@
 import GraphEdit from '../components/GraphEdit';
+import { useState } from 'react';
+import Button from '../components/Button';
 
 function GraphPage(){
+    const [graphs,setGraphs]=useState([]);
+
+    const handleClick=()=>{
+        setGraphs([...graphs,graphs.length]);
+    }
+
+    const renderedGraph=graphs.map(()=>{
+        return <div>
+            <GraphEdit />
+        </div>;
+    })
+
     return (
         <div>
             <h1>GraphPage</h1>
-            <GraphEdit />
+            <div>
+                <Button onClick={handleClick} primary outline rounded>Add Graph</Button>
+            </div>
+            {renderedGraph}
         </div>
     );
 }
